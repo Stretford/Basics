b4a3666865b4cb28426e1d1e80f7db16e3d45c3b
NStretford <stretford@Ns-MacBook-Pro.local>
2014-06-23 11:20:28 +0800

    Initial Commit

---
+++ Basics/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Basics
+//
+//  Created by NStretford on 6/23/14.
+//  Copyright (c) 2014 NStretford. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
