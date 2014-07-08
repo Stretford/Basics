65697f674796f7a31edc7f67c66968b09b6f6bb9
Stretford <stretford@Ns-MacBook-Pro.local>
2014-07-08 18:28:34 +0800

    Data Structure
    SingleLink, DoubleLink

---
+++ DoubleLink.c
@@ -0,0 +1,113 @@
+//
+//  DoubleLink.c
+//  Basics
+//
+//  Created by NStretford on 6/29/14.
+//  Copyright (c) 2014 NStretford. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dnode
+{
+    int data;
+    struct dnode *next;
+    struct dnode *pre;
+}dnode;
+
+dnode *create()
+{
+    dnode *head, *p, *s;
+    int x, cycle = 1;
+    head = (dnode *)malloc(sizeof(dnode));
+    p = head;
+    while(cycle)
+    {
+        printf("\nplease input data:");
+        scanf("%d", &x);
+        if(x != 0)
+        {
+            s = (dnode *)malloc(sizeof(dnode));
+            s->data = x;
+            printf("\n %d", s->data);
+            p->next = s;
+            s->pre = p;
+            p = s;
+        }
+        else
+            cycle = 0;
+    }
+    head = head->next;
+    head->pre = NULL;
+    p->next = NULL;
+    
+    printf("\n yyy %d", head->data);
+    return head;
+}
+
+dnode *del(dnode *head, int num)
+{
+    dnode *p1, *p2;
+    p1 = head;
+    while(num != p1->data && p1->next != NULL)
+        p1 = p1->next;
+    
+    if(num == p1->data)
+    {
+        if(p1 == head)
+        {
+            head = head->next;
+            head->pre = NULL;
+            free(p1);
+        }
+        else if(p1->next == NULL)
+        {
+            p1->pre->next = NULL;
+            free(p1);
+        }
+        else
+        {
+            p1->next->pre = p1->pre;
+            p1->pre->next = p1->next;
+            free(p1);
+        }
+    }
+    else
+        printf("\n%d could not be found", num);
+    return head;
+}
+
+dnode *insert(dnode *head, int num)
+{
+    dnode *p0, *p1;
+    p1 = head;
+    p0 = (dnode *)malloc(sizeof(dnode));
+    p0->data = num;
+    while(p0->data > p1->data && p1->next != NULL)
+        p1 = p1->next;
+    
+    if(p0->data <= p1->data)
+    {
+        if (head == p1)
+        {
+            p0->next = p1;
+            p1->pre = p0;
+            head = p0;
+        }
+        else
+        {
+            p1->pre->next = p0;
+            p0->next = p1;
+            p0->pre = p1->pre;
+            p1->pre = p0;
+        }
+    }
+    else
+    {
+        p1->next = p0;
+        p0->pre = p1;
+        p0->next = NULL;
+    }
+    return head;
+}
---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Basics
+//
+//  Created by NStretford on 6/23/14.
+//  Copyright (c) 2014 NStretford. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+//#include "SingleLink.c"
+extern void SLTest();
+
+int main(int argc, const char * argv[])
+{
+
+    SLTest();
+    return 0;
+}
+
